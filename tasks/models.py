b3e3bc693d52f19bbebdf56e96863d37cdbc78b1
Evgeniy <krasnoperoveg@mail.ru>
2024-05-05 11:12:26 +0200

    restore models

--- tasks/models.py
+++ tasks/models.py
@@ -11,7 +11,20 @@ class Test(models.Model):
 class Question(models.Model):
     test = models.ForeignKey(Test, related_name='questions', on_delete=models.CASCADE)
     text = models.TextField()
-    correct_answer = models.CharField(max_length=2)
 
     def __str__(self):
         return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+    text = models.TextField()
+    is_correct = models.BooleanField()
+
+
+class UserAnswer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.question.text}"
